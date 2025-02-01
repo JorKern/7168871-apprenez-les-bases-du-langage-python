f286b737c6520bec436dfcbd70aa7efd679ea66c
JorKern <JorKern-3071@proton.me>
2025-02-01 17:42:29 +0100

    Update main.py
    Exercice print / type en Python

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom="Louis"
+age=23
+taille=1.82
+est_étudiant=True
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_étudiant}")
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_étudiant)}")
